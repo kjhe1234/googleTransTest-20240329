516e931fbdc8824822929c7de85ea757e5768ed1
kjhe1234 <kjhe1234@naver.com>
2024-03-29 16:16:48 +0900

    Qt 추가

--- googleTransTest.py
+++ googleTransTest.py
@@ -1,18 +1,19 @@
 import googletrans
 
-trans = googletrans.Translator()  # 구글 번역 객체 생성
-result1 = trans.translate("오늘은 비가 왔어", dest="en")  # 영어로 번역
-#print(googletrans.LANGUAGES)  -> 번역 언어의 dest 약자 찾기
-#result2 = trans.translate("오늘은 비가 왔어", dest="ja")  # 일본어로 번역
-#result3 = trans.translate("오늘은 비가 왔어", dest="zh-cn") # 중국어로 번역
-#result4 = trans.translate("오늘은 비가 왔어", dest="ro")  # 로마어로 번역
-#result5 = trans.translate("오늘은 비가 왔어", dest="ru")  # 러시아어로 번역
+while True:
+    korStr = input("번역할 문장을 입력하세요:")
+    if korStr == "끝":
+        break
+    trans = googletrans.Translator()
+    result1 = trans.translate(korStr, dest="en")  # 영어로 번역
+    result2 = trans.translate(korStr, dest="ja")  # 일본어로 번역
+    result3 = trans.translate(korStr, dest="zh-cn")  # 중국어로 번역
+    print(f"입력하신 [{korStr}은 영어로 [{result1.text}] 입니다")
+    print(f"입력하신 [{korStr}은 일본어로 [{result2.text}] 입니다")
+    print(f"입력하신 [{korStr}은 중국어로 [{result3.text}] 입니다")
 
-print(result1.text)
-#print(result2.text)
-#print(result3.text)
-#print(result4.text)
-#print(result5.text)
+  # 구글 번역 객체 생성
+#print(googletrans.LANGUAGES)  -> 번역 언어의 dest 약자 찾기
 
 
 
